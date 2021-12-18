b5b8a59ca81bf89cdd83fce15ff9461eb85a15bc
Almustapha Sulaiman <almustaphasulaiman@mailbox.unideb.hu>
2021-12-18 05:43:50 +0100

    Login adjustments

---
+++ app/src/main/java/com/asma/onlinequiz/LoginFragment.java
@@ -0,0 +1,142 @@
+package com.asma.onlinequiz;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.concurrent.Executor;
+
+
+public class LoginFragment extends DialogFragment{
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this.getContext(), gso);
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this.getContext());
+        final View view;
+        if(acct != null){
+            view = inflater.inflate(R.layout.account_info, container, false);
+            TextView displayName = view.findViewById(R.id.displayNameTextView);
+            displayName.setText(acct.getDisplayName());
+            //TextView accountName = view.findViewById(R.id.accountNameTextView);
+            //accountName.setText(acct.getGivenName());
+            TextView email = view.findViewById(R.id.accountEmailTextView);
+            email.setText(acct.getEmail());
+            Uri personPhoto = acct.getPhotoUrl();
+            ImageView proPic = view.findViewById(R.id.profilePicImageView);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(proPic);
+            //proPic.setImageURI(acct.getPhotoUrl());
+
+            view.findViewById(R.id.accountSignOutButton).setOnClickListener(v -> {
+                signOut();
+                revokeAccess();
+            });
+
+        }
+        else {
+            // Inflate the layout for this fragment
+            view = inflater.inflate(R.layout.fragment_login, container, false);
+            view.findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Login", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            view.findViewById(R.id.cancelLoginButton).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+            view.findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    signIn();
+                }
+            });
+        }
+
+        return view;
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,40);
+        dismiss();
+    }
+
+    private void signOut() {
+        dismiss();
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getContext(), "Account Signed Out", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                    }
+                });
+    }
+}
